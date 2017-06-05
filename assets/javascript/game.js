9d55cdc309d5d7e721dff48603eeab30e7c06875
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-06-04 22:34:18 -0400

    add pictures and begin javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,41 @@
-//Use the src attribute of the script tag to link to this file, rather than embedding the code directly in your HTML document.
+var mysteryWord [
+        "Beauty and the Beast",
+        "Cinderella", 
+        "The Ginger Bread Man", 
+        "Goldilocks and the Three Bears", 
+        "The Little Mermaid", 
+        "The Little Red Hen", 
+        "Sleeping Beauty",
+        "Stone Soup",
+        "Three Billy Goats Gruff", 
+        "Three Little Pigs",
+        "The Ugly Duckling"];
+
+
+var picture = [ 
+        "assets/images/beautyBeast.jpg",
+        "assets/images/cinderella.jpg",
+        "assets/images/gingerbread-man.png",
+        "assets/images/goldilocks.jpg",
+        "assets/images/littlemermaid.jpg",
+        "assets/images/hen.jpg",
+        "assets/images/sleeping-beauty.jpg",
+        "assets/images/stone-soup.jpg",
+        "assets/images/goats.jpg",
+        "assets/images/Three_Little_Pigs.png",
+        "assets/images/ugly.png"];
+
+
+var win = 0; new Audio('assets/images/tinkle.mp3'); 
+var lose = 0, new Audio('assets/images/evil.mp3');
+
+
+var currentWord; 
+var guesses = []; 
+var selectedWord = [];
+var guessesRemain = 8; 
+var guess = ''; guesses;
+var correctCount = 0;
+var pickedWord = 0;
+var alreadyGuessed = [];
+
